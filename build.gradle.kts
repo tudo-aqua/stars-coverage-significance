ddb8fdc972805c8d1b71cea86059bc0e7db6e7a0
Till Schallau <till.schallau@tu-dortmund.de>
2025-07-16 22:59:42 +0200

    Add base project based on stars-carla-experiments

---
+++ build.gradle.kts
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2023-2025 The STARS Coverage Significance Authors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+  kotlin("jvm") version "2.1.10"
+  application
+  id("io.gitlab.arturbosch.detekt") version "1.23.6"
+  id("com.diffplug.spotless") version "7.0.2"
+}
+
+group = "tools.aqua"
+
+version = "0.5"
+
+repositories { mavenCentral() }
+
+var starsVersion = "0.5"
+
+dependencies {
+  testImplementation(kotlin("test-junit5"))
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+  testImplementation(group = "tools.aqua", name = "stars-core", configuration = "tests")
+  testImplementation(kotlin("test"))
+  implementation(group = "tools.aqua", name = "stars-core", version = starsVersion)
+  implementation(group = "tools.aqua", name = "stars-logic-kcmftbl", version = starsVersion)
+  implementation(group = "tools.aqua", name = "stars-data-av", version = starsVersion)
+  implementation(group = "tools.aqua", name = "stars-importer-carla", version = starsVersion)
+  implementation(group = "com.github.ajalt.clikt", name = "clikt", version = "5.0.2")
+  detektPlugins(
+      group = "io.gitlab.arturbosch.detekt", name = "detekt-rules-libraries", version = "1.23.6")
+}
+
+detekt {
+  basePath = rootProject.projectDir.absolutePath
+  config.setFrom(files(rootProject.file("contrib/detekt-rules.yml")))
+}
+
+spotless {
+  kotlin {
+    licenseHeaderFile(rootProject.file("contrib/license-header.template.kt")).also {
+      it.updateYearWithLatest(true)
+    }
+    ktfmt()
+  }
+  kotlinGradle {
+    licenseHeaderFile(
+            rootProject.file("contrib/license-header.template.kt"),
+            "(import |@file|plugins |dependencyResolutionManagement|rootProject.name)")
+        .also { it.updateYearWithLatest(true) }
+    ktfmt()
+  }
+}
+
+tasks.test { useJUnitPlatform() }
+
+application { mainClass.set("tools.aqua.stars.carla.experiments.Experiment") }
+
+kotlin { jvmToolchain(17) }
---
+++ src/main/kotlin/tools/aqua/stars/coverage/significance/ExperimentConfiguration.kt
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2025 The STARS Coverage Significance Authors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tools.aqua.stars.coverage.significance
+
+import java.io.File
+import java.nio.file.Path
+import kotlin.io.path.name
+import tools.aqua.stars.core.evaluation.TSCEvaluation
+import tools.aqua.stars.core.metric.metrics.evaluation.*
+import tools.aqua.stars.core.metric.metrics.postEvaluation.*
+import tools.aqua.stars.data.av.dataclasses.*
+import tools.aqua.stars.data.av.metrics.AverageVehiclesInEgosBlockMetric
+import tools.aqua.stars.importer.carla.CarlaSimulationRunsWrapper
+import tools.aqua.stars.importer.carla.loadSegments
+
+fun main() {
+  val tsc = tsc()
+
+  println("Loading simulation runs...")
+  val simulationRunsWrapper = loadSingleExperiment("simulations")
+
+  println("Loading segments...")
+  val segments =
+      loadSegments(
+          simulationRunsWrappers = listOf(simulationRunsWrapper),
+      )
+
+  val validTSCInstancesPerProjectionMetric =
+      ValidTSCInstancesPerTSCMetric<
+          Actor, TickData, Segment, TickDataUnitSeconds, TickDataDifferenceSeconds>()
+
+  println("Creating TSC...")
+  val evaluation =
+      TSCEvaluation(tscList = listOf(tsc())).apply {
+        registerMetricProviders(
+            TotalSegmentTickDifferencePerIdentifierMetric(),
+            SegmentCountMetric(),
+            AverageVehiclesInEgosBlockMetric(),
+            TotalSegmentTickDifferenceMetric(),
+            validTSCInstancesPerProjectionMetric,
+            InvalidTSCInstancesPerTSCMetric(),
+            MissedTSCInstancesPerTSCMetric(),
+            MissedPredicateCombinationsPerTSCMetric(validTSCInstancesPerProjectionMetric),
+            FailedMonitorsMetric(validTSCInstancesPerProjectionMetric),
+        )
+        println("Run Evaluation")
+        runEvaluation(segments = segments)
+      }
+}
+
+fun loadSingleExperiment(
+    folderName: String,
+    staticFilter: String = ".*",
+    dynamicFilter: String = ".*"
+): CarlaSimulationRunsWrapper {
+  var staticFile: Path? = null
+  val dynamicFiles = mutableListOf<Path>()
+  val mapFolder = File(folderName)
+  mapFolder.walk().forEach { mapFile ->
+    if (mapFile.nameWithoutExtension.contains("static_data") &&
+        staticFilter.toRegex().containsMatchIn(mapFile.name)) {
+      staticFile = mapFile.toPath()
+    }
+    if (mapFile.nameWithoutExtension.contains("dynamic_data") &&
+        dynamicFilter.toRegex().containsMatchIn(mapFile.name)) {
+      dynamicFiles.add(mapFile.toPath())
+    }
+  }
+
+  checkNotNull(staticFile) { "Static data file not found." }
+  check(dynamicFiles.isNotEmpty()) { "Dynamic data file not found." }
+
+  dynamicFiles.sortBy {
+    "_seed([0-9]{1,4})".toRegex().find(it.fileName.name)?.groups?.get(1)?.value?.toInt() ?: 0
+  }
+  return CarlaSimulationRunsWrapper(staticFile, dynamicFiles)
+}
