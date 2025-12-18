3e9c14abe627ed552d4a8047635786aa69185ca1
Till Schallau <till.schallau@tu-dortmund.de>
2025-12-18 11:08:03 +0100

    Update license header

--- contrib/license-header.template.md
+++ contrib/license-header.template.md
@@ -1,5 +1,5 @@
 <!--
- Copyright $YEAR The STARS OWA Coverage Project Authors
+ Copyright $YEAR The STARS Coverage Significance Project Authors
  SPDX-License-Identifier: Apache-2.0
 
  Licensed under the Apache License, Version 2.0 (the "License");
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2025 The STARS OWA Coverage Authors
+ * Copyright 2023-2025 The STARS Coverage Significance Authors
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
