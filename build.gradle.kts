e133bb34633b8d40484d9a9b8edefaf61464f380
Till Schallau <till.schallau@tu-dortmund.de>
2026-01-19 02:17:14 +0100

    Update starsVersion

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,23 +17,6 @@
 
 import java.net.URI
 
-/*
- * Copyright 2023-2025 The STARS OWA Coverage Authors
- * SPDX-License-Identifier: Apache-2.0
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 plugins {
   kotlin("jvm") version "2.2.0"
   application
@@ -47,7 +30,7 @@ repositories {
   maven { url = URI("https://central.sonatype.com/repository/maven-snapshots/") }
 }
 
-val starsVersion = "2.0-coverage-significance-13-aab61e3-SNAPSHOT"
+val starsVersion = "2.0-coverage-significance-15-59d7704-SNAPSHOT"
 
 dependencies {
   testImplementation(kotlin("test-junit5"))
