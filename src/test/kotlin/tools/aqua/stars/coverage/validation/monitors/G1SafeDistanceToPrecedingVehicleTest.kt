a9e7de7160ba04eca27dbe60f9e2033ce67ffbd4
Till Schallau <till.schallau@tu-dortmund.de>
2026-01-25 14:18:44 +0100

    Add tests for cut-in detection and lane change scenarios in G1SafeDistanceToPrecedingVehicle

--- src/test/kotlin/tools/aqua/stars/coverage/validation/monitors/G1SafeDistanceToPrecedingVehicleTest.kt
+++ src/test/kotlin/tools/aqua/stars/coverage/validation/monitors/G1SafeDistanceToPrecedingVehicleTest.kt
@@ -20,6 +20,9 @@ package tools.aqua.stars.coverage.validation.monitors
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import tools.aqua.stars.core.evaluation.TickSequence.Companion.asTickSequence
+import tools.aqua.stars.coverage.significance.cutIn
+import tools.aqua.stars.coverage.significance.isBesidesOf
+import tools.aqua.stars.coverage.significance.isInFrontOfAbsolute
 import tools.aqua.stars.coverage.significance.isOnSameLane
 import tools.aqua.stars.coverage.validation.predicates.PredicateTestHelper
 import tools.aqua.stars.coverage.validation.predicates.PredicateTestHelper.getTestTimeStep
@@ -28,17 +31,19 @@ import tools.aqua.stars.coverage.validation.predicates.leftLane
 import tools.aqua.stars.coverage.validation.predicates.middleLane
 import tools.aqua.stars.logic.kcmftbl.past.previous
 
+/** Test class for G1: Safe Distance to Preceding Vehicle monitor. */
 class G1SafeDistanceToPrecedingVehicleTest {
 
+  /** Test correct cut-in detection. */
   @Test
   fun `Test correct cut-in detection`() {
     val roadNetwork = RoadNetworkTestHelpers.threeLaneSingleEdgeNetwork()
-    val ego1 = PredicateTestHelper.getTestVehicle("v1", roadNetwork.middleLane, 0.0f)
-    val otherVehicle1 = PredicateTestHelper.getTestVehicle("v2", roadNetwork.leftLane, 0.0f)
+    val ego1 = PredicateTestHelper.getTestVehicle("ego", roadNetwork.middleLane, 20.0f)
+    val otherVehicle1 = PredicateTestHelper.getTestVehicle("other", roadNetwork.leftLane, 20.0f)
     val tick1 = getTestTimeStep(listOf(ego1, otherVehicle1), ego = ego1)
 
-    val ego2 = PredicateTestHelper.getTestVehicle("v1", roadNetwork.leftLane, 0.0f)
-    val otherVehicle2 = PredicateTestHelper.getTestVehicle("v2", roadNetwork.leftLane, 0.0f)
+    val ego2 = PredicateTestHelper.getTestVehicle("ego", roadNetwork.leftLane, 25.0f)
+    val otherVehicle2 = PredicateTestHelper.getTestVehicle("other", roadNetwork.leftLane, 30.0f)
     val tick2 = getTestTimeStep(listOf(ego2, otherVehicle2), ego = ego2)
 
     val ticks = listOf(tick1, tick2).asTickSequence().toList()
@@ -54,8 +59,95 @@ class G1SafeDistanceToPrecedingVehicleTest {
             firstTick, secondTick.ego to secondTick.getVehicleById(otherVehicle2.vehicleId)))
     assert(
         previous(secondTick) { tick ->
-          !isOnSameLane.holds(
-              tick, firstTick.ego to firstTick.getVehicleById(otherVehicle1.vehicleId))
+          !isOnSameLane.holds(tick, tick.ego to tick.getVehicleById(otherVehicle1.vehicleId))
         })
+    assert(
+        previous(secondTick) { tick ->
+          isBesidesOf.holds(tick, tick.ego to tick.getVehicleById(otherVehicle1.vehicleId))
+        })
+    assert(
+        isInFrontOfAbsolute.holds(
+            secondTick, secondTick.getVehicleById(otherVehicle2.vehicleId) to secondTick.ego))
+    assert(
+        cutIn.holds(
+            secondTick, secondTick.ego to secondTick.getVehicleById(otherVehicle2.vehicleId)))
+  }
+
+  /** Test that no cut-in is detected when there is no lane change. */
+  @Test
+  fun `Test cut-in without lane change`() {
+    val roadNetwork = RoadNetworkTestHelpers.threeLaneSingleEdgeNetwork()
+    val ego1 = PredicateTestHelper.getTestVehicle("ego", roadNetwork.middleLane, 20.0f)
+    val otherVehicle1 = PredicateTestHelper.getTestVehicle("other", roadNetwork.leftLane, 20.0f)
+    val tick1 = getTestTimeStep(listOf(ego1, otherVehicle1), ego = ego1)
+
+    val ego2 = PredicateTestHelper.getTestVehicle("ego", roadNetwork.middleLane, 25.0f)
+    val otherVehicle2 = PredicateTestHelper.getTestVehicle("other", roadNetwork.leftLane, 30.0f)
+    val tick2 = getTestTimeStep(listOf(ego2, otherVehicle2), ego = ego2)
+
+    val ticks = listOf(tick1, tick2).asTickSequence().toList()
+    val firstTick = ticks[1]
+    val secondTick = ticks[1].nextTick
+    checkNotNull(secondTick)
+    assertEquals(secondTick, tick2)
+    assert(
+        !isOnSameLane.holds(
+            firstTick, firstTick.ego to firstTick.getVehicleById(otherVehicle1.vehicleId)))
+    assert(
+        !isOnSameLane.holds(
+            firstTick, secondTick.ego to secondTick.getVehicleById(otherVehicle2.vehicleId)))
+    assert(
+        previous(secondTick) { tick ->
+          !isOnSameLane.holds(tick, tick.ego to tick.getVehicleById(otherVehicle1.vehicleId))
+        })
+    assert(
+        previous(secondTick) { tick ->
+          isBesidesOf.holds(tick, tick.ego to tick.getVehicleById(otherVehicle1.vehicleId))
+        })
+    assert(
+        isInFrontOfAbsolute.holds(
+            secondTick, secondTick.getVehicleById(otherVehicle2.vehicleId) to secondTick.ego))
+    assert(
+        !cutIn.holds(
+            secondTick, secondTick.ego to secondTick.getVehicleById(otherVehicle2.vehicleId)))
+  }
+
+  /** Test that no cut-in is detected when cut-in is behind ego. */
+  @Test
+  fun `Test cut-in with cut-in behind ego`() {
+    val roadNetwork = RoadNetworkTestHelpers.threeLaneSingleEdgeNetwork()
+    val ego1 = PredicateTestHelper.getTestVehicle("ego", roadNetwork.middleLane, 20.0f)
+    val otherVehicle1 = PredicateTestHelper.getTestVehicle("other", roadNetwork.leftLane, 20.0f)
+    val tick1 = getTestTimeStep(listOf(ego1, otherVehicle1), ego = ego1)
+
+    val ego2 = PredicateTestHelper.getTestVehicle("ego", roadNetwork.leftLane, 30.0f)
+    val otherVehicle2 = PredicateTestHelper.getTestVehicle("other", roadNetwork.leftLane, 25.0f)
+    val tick2 = getTestTimeStep(listOf(ego2, otherVehicle2), ego = ego2)
+
+    val ticks = listOf(tick1, tick2).asTickSequence().toList()
+    val firstTick = ticks[1]
+    val secondTick = ticks[1].nextTick
+    checkNotNull(secondTick)
+    assertEquals(secondTick, tick2)
+    assert(
+        !isOnSameLane.holds(
+            firstTick, firstTick.ego to firstTick.getVehicleById(otherVehicle1.vehicleId)))
+    assert(
+        isOnSameLane.holds(
+            firstTick, secondTick.ego to secondTick.getVehicleById(otherVehicle2.vehicleId)))
+    assert(
+        previous(secondTick) { tick ->
+          !isOnSameLane.holds(tick, tick.ego to tick.getVehicleById(otherVehicle1.vehicleId))
+        })
+    assert(
+        previous(secondTick) { tick ->
+          isBesidesOf.holds(tick, tick.ego to tick.getVehicleById(otherVehicle1.vehicleId))
+        })
+    assert(
+        !isInFrontOfAbsolute.holds(
+            secondTick, secondTick.getVehicleById(otherVehicle2.vehicleId) to secondTick.ego))
+    assert(
+        !cutIn.holds(
+            secondTick, secondTick.ego to secondTick.getVehicleById(otherVehicle2.vehicleId)))
   }
 }
