54b853a2e078f11e8f2bea0ac96495a29978870b
Till Schallau <till.schallau@tu-dortmund.de>
2026-01-27 05:03:15 +0100

    Create all generated scenarios as chunks

--- src/main/kotlin/tools/aqua/stars/coverage/significance/CreateChunkJobs.kt
+++ src/main/kotlin/tools/aqua/stars/coverage/significance/CreateChunkJobs.kt
@@ -38,5 +38,8 @@ fun main() {
   println("Seeding chunk jobs...")
   val numberOfScenarios = ScenarioStartingConfigurationRepository.getMaxSequenceNumber()
   ChunkJobSeeder.seedChunks(
-      runId = evaluationRunId, mutantIds = mutantIds, chunkSize = 1_000L, scenarioCount = numberOfScenarios)
+      runId = evaluationRunId,
+      mutantIds = mutantIds,
+      chunkSize = 1_000L,
+      scenarioCount = numberOfScenarios)
 }
