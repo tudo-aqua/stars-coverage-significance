d0d3be183af6af7c996c6865db06dbd0097f7fbf
Till Schallau <till.schallau@tu-dortmund.de>
2026-02-18 16:30:36 +0100

    For test purposes: Only seed minimal chunks

--- src/main/kotlin/tools/aqua/stars/coverage/significance/CreateChunkJobs.kt
+++ src/main/kotlin/tools/aqua/stars/coverage/significance/CreateChunkJobs.kt
@@ -45,6 +45,6 @@ fun main() {
   ChunkJobSeeder.seedChunks(
       runId = evaluationRunId,
       mutantIds = mutantIds,
-      chunkSize = 1_000L,
-      scenarioCount = numberOfScenarios)
+      chunkSize = 1L,
+      scenarioCount = 1)
 }
