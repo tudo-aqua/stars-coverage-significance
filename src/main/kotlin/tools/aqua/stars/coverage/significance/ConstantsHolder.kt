a3adc8d6a819107b5346baedadaf7697a85db27a
Till Schallau <till.schallau@tu-dortmund.de>
2026-02-18 14:40:24 +0100

    Change max length of simulations to 10s

--- src/main/kotlin/tools/aqua/stars/coverage/significance/ConstantsHolder.kt
+++ src/main/kotlin/tools/aqua/stars/coverage/significance/ConstantsHolder.kt
@@ -42,6 +42,6 @@ const val BUFFER_SIZE_IN_SECONDS = 10.0
 /** When importing tick sequences, only take ticks at every X milliseconds. This reduces memory */
 const val TAKE_ONLY_TICKS_AT_X_MILLIS = 100
 /** Maximum length of a scenario in seconds. */
-const val MAX_LENGTH_OF_SCENARIO_IN_SECONDS = 20.0
+const val MAX_LENGTH_OF_SCENARIO_IN_SECONDS = 10.0
 /** Size of the buffer (in number of ticks) to use when importing tick sequences. */
 const val BUFFER_SIZE = ((BUFFER_SIZE_IN_SECONDS * 1000) / TAKE_ONLY_TICKS_AT_X_MILLIS).toInt()
