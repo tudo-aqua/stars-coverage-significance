dbae59be2792b0d0de2142fa01fb18d49b0ab78e
Till Schallau <till.schallau@tu-dortmund.de>
2026-02-28 15:03:32 +0100

    Insert all mutants

--- src/main/kotlin/tools/aqua/stars/coverage/significance/PrepareDatabaseAndSeedWithScenariosAndMutants.kt
+++ src/main/kotlin/tools/aqua/stars/coverage/significance/PrepareDatabaseAndSeedWithScenariosAndMutants.kt
@@ -54,7 +54,7 @@ fun main() {
   seedGridTrafficScenarios(seed = SEED, insertIntoDatabase = true, enablePositionVariance = false)
 
   // Seed mutants
-  seedMutants(seedBaseLine = true, seedMutants = false)
+  seedMutants(seedBaseLine = true, seedMutants = true)
 
   // Precompute scenario-only metric once
   runStartingValidTSCInstancesEvaluation(parallelism = parallelism - 2, tscId = tscId)
