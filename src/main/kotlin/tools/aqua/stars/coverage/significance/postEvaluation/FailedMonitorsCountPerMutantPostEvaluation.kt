84a19aef23eae32f04a3ccdfd039512743f6009a
Till Schallau <till.schallau@tu-dortmund.de>
2026-03-03 10:28:34 +0100

    Fix detekt warning

--- src/main/kotlin/tools/aqua/stars/coverage/significance/postEvaluation/FailedMonitorsCountPerMutantPostEvaluation.kt
+++ src/main/kotlin/tools/aqua/stars/coverage/significance/postEvaluation/FailedMonitorsCountPerMutantPostEvaluation.kt
@@ -85,13 +85,15 @@ object FailedMonitorsCountPerMutantPostEvaluation {
     val folder = POST_EVALUATION_BASE_DIR
     val subfolder = "failed_monitors_count_per_mutant"
 
-    listOf(1, 10, 25, 50, 100, 200).forEach { everyNthEntry ->
+    listOf(1).forEach { everyNthEntry ->
       val everyNThSubfolder = "every_${everyNthEntry}-th_entry"
       val csvFileName = "${subfolder}-every_${everyNthEntry}-th_entry.csv"
       val texFileName = "${subfolder}-every_${everyNthEntry}-th_entry.tex"
+      val largeTexFileName = "${subfolder}-every_${everyNthEntry}-th_entry-large.tex"
 
       val csvPath = Path.of(folder, subfolder, everyNThSubfolder, csvFileName)
       val texPath = Path.of(folder, subfolder, everyNThSubfolder, texFileName)
+      val largeTexPath = Path.of(folder, subfolder, everyNThSubfolder, largeTexFileName)
 
       Files.createDirectories(csvPath.parent)
 
@@ -99,6 +101,8 @@ object FailedMonitorsCountPerMutantPostEvaluation {
       csvPath.writeText(csv)
       val tex = buildTexString(csvFileName, everyNthEntry)
       texPath.writeText(tex)
+      val largeTex = buildLargeTexString(csvFileName, everyNthEntry)
+      largeTexPath.writeText(largeTex)
     }
   }
 
@@ -116,16 +120,52 @@ object FailedMonitorsCountPerMutantPostEvaluation {
     
     \begin{tikzpicture}
         \begin{axis}[
-            ybar,
+            ybar, 
             bar width=1pt,
             title={Failed Monitors per Mutant (every $${everyNthEntry}^{th}$ entry)},
             xlabel={Mutant},
             ylabel={Failed Monitors},
-            enlargelimits=0.02,
+            enlarge x limits=false,
+            ymin=0,
+            xtick distance=50,
+            xticklabel style={rotate=45, anchor=east},
+            scaled y ticks=false,
+            ]
+            \addplot table[
+            x expr=\coordindex,
+            y=count,
+            ]{\datatable};
+        \end{axis}
+    \end{tikzpicture}
+    
+\end{document}
+    """
+
+  private fun buildLargeTexString(csvFileName: String, everyNthEntry: Int) =
+    """
+\documentclass[tikz,border=5pt]{standalone}
+\usepackage{pgfplots}
+\pgfplotsset{compat=1.18}
+\usepackage{pgfplotstable}
+
+\begin{document}
+    
+    % Read CSV once with the correct separator:
+    \pgfplotstableread[col sep=comma]{${csvFileName}}\datatable
+    
+    \begin{tikzpicture}
+        \begin{axis}[
+            ybar,
+            width=80cm,
+            bar width=8pt,
+            title={Failed Monitors per Mutant (every $${everyNthEntry}^{th}$ entry)},
+            xlabel={Mutant},
+            ylabel={Failed Monitors},
             ymin=0,
             xtick=data,
             xticklabel style={rotate=45, anchor=east},
             xticklabels from table={\datatable}{mutantId},
+            enlarge x limits=false,
             tick label style={font=\small},
             scaled y ticks=false,
             ]
