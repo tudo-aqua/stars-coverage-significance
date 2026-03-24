5903d11c976c5bcb2c97ac588a549756048c95e6
Dominik Schmid <dominik.schmid@tu-dortmund.de>
2026-03-24 15:03:54 +0100

    Add additional repetitions in MutantKillingPostEvaluation.

--- src/main/kotlin/tools/aqua/stars/coverage/significance/postEvaluation/MutantKillingPostEvaluation.kt
+++ src/main/kotlin/tools/aqua/stars/coverage/significance/postEvaluation/MutantKillingPostEvaluation.kt
@@ -36,7 +36,7 @@ import tools.aqua.stars.coverage.significance.smallStaticTsc
 object MutantKillingPostEvaluation {
 
   val TSC_SIZE = smallStaticTsc().instanceCount.toInt()
-  val REPETITIONS: List<Int> = listOf(1,2,3,4,5) * TSC_SIZE
+  val REPETITIONS: List<Int> = listOf(1, 2, 3, 4, 5, 8, 10) * TSC_SIZE
 
   typealias Repetitions = Int
 
